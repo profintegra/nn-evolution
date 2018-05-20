447678939af33a0aab48f79d1eb4a266b6c862ae
Marlon Flügge <marlonfl@fb3.uni-bremen.de>
2018-05-20 12:17:36 +0200

    add evo alg version that can handle nn parameter shape

--- evolution/evolution.py
+++ evolution/evolution.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class EvolutionGeneric():
+class GenericEvolution():
     
     def __init__(self, genes, pop_size, fitness_func, lr=0.1, generations=100, 
                     base_indv=None):
@@ -53,3 +53,74 @@ class EvolutionGeneric():
             self.best_indv = mutated_population[max_idx]
             self.max_fitness = fitness_scores[max_idx]
     
+    
+class FitnessBasedSamplingEvolution():
+    def __init__(self, model, pop_size, fitness_func, noise_mean=0,
+                    noise_std=0.1, generations=10):
+        self.model = model
+        self.pop_size = pop_size
+        self.calc_fitness = fitness_func
+        self.generations = generations
+        self.noise_std = noise_std
+        self.noise_mean = noise_mean
+
+        self.population = None
+        self.max_fitness = -1
+        self.best_indv = None
+        
+        self.base_indv = np.array([p.data.numpy() for p in model.parameters()])
+        self.p_shapes = np.array([p.shape for p in self.base_indv])
+        
+    def init_population(self):
+        return np.array([self.base_indv] * self.pop_size)
+    
+    def generate_mutations(self):
+        mutations = []
+        for indv in self.pop_size:
+            m = []
+            for shape in self.p_shapes:
+                m.append(np.random.normal(
+                            loc=self.noise_mean, scale=self.noise_std, 
+                            size=shape
+                            ))
+            mutations.append(np.array(m))
+        return np.array(mutations)
+        
+    def evolve(self):
+        if self.population is None:
+            self.population = self.init_population()
+            
+        for gen in range(self.generations):
+            # mutating population
+            mutations = self.generate_mutations()
+            mutated_pop = self.population + mutations 
+            
+            # calculating fitness scores for resulting individuals, saving best
+            fitness_scores = []
+            for indv in mutated_pop:
+                self.update_model_params(indv)
+                fitness_scores.append(self.calc_fitness())
+            fitness_scores = [self.calc_fitness() for indv in mutated_pop]
+            self.update_best(fitness_scores, mutated_population)
+            
+            # sampling from mutation population based on fitness
+            fitness_score_prob = fitness_scores / sum(fitness_scores)
+            new_population_idx = np.random.choice(
+                                            len(mutated_pop), 
+                                            p=fitness_score_prob, 
+                                            size=self.pop_size
+                                            )
+                                                  
+            self.population = mutated_pop[new_population_idx]
+        
+        self.update_model_params(self.best_indv)
+
+    def update_best(self, fitness_scores, mutated_population):
+        max_idx = fitness_scores.index(max(fitness_scores))
+        if fitness_scores[max_idx] > self.max_fitness:
+            self.best_indv = mutated_population[max_idx]
+            self.max_fitness = fitness_scores[max_idx]
+    
+    def update_model_params(self, indv):
+        for i, p in enumerate(self.model.parameters()):
+            p.data = torch.from_numpy(indv[i])
